8a3d2f760375ba546f92923192b8718e30fbd62b
Djon <latiiodessa@gmail.com>
2020-11-16 19:19:23 +0200

    map modal

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,12 +5,13 @@ import Header from '../../components/header/Header'
 /* import Contact from '../contact/Contact'; */
 import ContactList from '../contact/contactList/ContactList';
 import Contact from '../contact/Contact';
-import {UsePositionDemo} from '../../components/map/UsePositionDemo';
+
 
 import {
   Route,
   Switch,
 } from "react-router-dom"
+import ModalMap from '../../components/map/ModalMap';
 
 export default class Home extends React.Component {
     render() {
@@ -21,7 +22,7 @@ export default class Home extends React.Component {
           <Route path='/dialog' component={Dialog} />
           <Route path='/contact' component={Contact} />
           <Route path='/contactList' component={ContactList} />
-          <Route path='/map' component={UsePositionDemo} />
+          <Route path='/map' component={ModalMap} />
 
         </Switch>
         </>
