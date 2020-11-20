eb21b7d6ebb7c4dae5d6cba58d3367d84d56908a
Nicelx <Relush321@gmail.com>
2020-11-20 19:49:53 +0300

    still wanna change smth

--- src/pages/dialog/Dialog.js
+++ src/pages/dialog/Dialog.js
@@ -10,91 +10,76 @@ import { Container, CircularProgress } from '@material-ui/core';
 
 import classes from './Dialog.module.css';
 
-
-const Dialog = props => {
-  const [dialogHeight] = useState(window.innerHeight)
+const Dialog = (props) => {
+  const [dialogHeight] = useState(window.innerHeight);
   const [messages, setMessages] = useState('');
-  const [personInfo, setPersonInfo] = useState('')
+  const [personInfo, setPersonInfo] = useState('');
   const [loaded, setLoaded] = useState(false);
-  const [uid] = useState(props.user.uid);
   // myRef = React.createRef();
 
-  const dbUsers = database.ref().child('users')
+  const dbUsers = database.ref().child('users');
   const dbMessages = database.ref().child('messages/first');
-
-  useEffect(() => {
-    dbMessages.on('value', snap => {
-      setMessages(snap.val());
-      
-    })
-    dbUsers.once('value', snap => {
-      setPersonInfo(snap.val())
-      setLoaded(true);
-    })
-    return () =>{
-      dbMessages.off();
-    }
-    }, [])
-    
   
+  useEffect(() => {
+    dbMessages.on('value', (snap) => {
+      setMessages(snap.val());
+    });
+    dbUsers.once('value', (snap) => {
+      setPersonInfo(snap.val());
+      setLoaded(true);
+    });
+    return () => {
+      dbMessages.off();
+    };
+    // crutch here)))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const sendMessageHandler = (messageValue) => {
-    const objToPush = {}
+    const objToPush = {};
     objToPush.message = messageValue;
     objToPush.timestamp = new Date().getTime();
-    objToPush.type = 'text'
+    objToPush.type = 'text';
     objToPush.user = props.user.uid;
-    dbMessages.push(objToPush)
+    dbMessages.push(objToPush);
   };
-    
+
   //still workin on that
   // setScroll = () => {
   //   if (this.myRef) this.myRef.current.scrollTop = this.myRef.current.scrollHeight;
   // };
-    return (
-      <Container
-        style={{ height: dialogHeight - 68 }}
-        className={classes.Container}
-      >
-        <div className={classes.Dialog}>
-          <div className={classes.DialogField}>
-            <h3>Welcome</h3>
-            {!loaded && <CircularProgress className = {classes.Progress}/>}
-            {loaded && Object.keys(messages).map((messageItemKey, index) => {
-              const props = {}
-              const name = personInfo[messages[messageItemKey].user].name
-              const avatar = personInfo[messages[messageItemKey].user].avatar
+  return (
+    <Container style={{ height: dialogHeight - 68 }} className={classes.Container}>
+      <div className={classes.Dialog}>
+        <div className={classes.DialogField}>
+          <h3>Welcome</h3>
+          {!loaded && <CircularProgress className={classes.Progress} />}
+          {loaded &&
+            Object.keys(messages).map((messageItemKey,index) => {
+              const ItemProps = {};
+              const name = personInfo[messages[messageItemKey].user].name;
+              const avatar = personInfo[messages[messageItemKey].user].avatar;
 
-              props.key = messageItemKey
-              props.value = messages[messageItemKey].message
-              props.name = name
-                ? name
-                : 'NoName'
-              props.time = new Date(messages[messageItemKey].timestamp).toLocaleTimeString()
-              props.avatar = avatar
+              ItemProps.key = messageItemKey;
+              ItemProps.value = messages[messageItemKey].message;
+              ItemProps.name = name ? name : 'NoName';
+              ItemProps.time = new Date(
+                messages[messageItemKey].timestamp
+              ).toLocaleTimeString();
+              ItemProps.avatar = avatar
                 ? avatar
-                : 'https://124ural.ru/wp-content/uploads/2017/04/no-avatar.png' // default avatar
-
-              // weird thing below.... props.user.uid - undefined.......
-              if (uid === messages[messageItemKey].user)
-              return (
-                <MessageItemYours {...props} />
-              ) 
-              else return (
-                <MessageItemNotYours {...props} />
-              )
-          })
-          }
-
-          
-          </div>
-
-          <InputTextArea
-            sendMessage={sendMessageHandler}
-          />
+                : 'https://124ural.ru/wp-content/uploads/2017/04/no-avatar.png'; // default avatar
+              if (props.user.uid === messages[messageItemKey].user)
+                return <MessageItemYours {...ItemProps} />;
+              else return <MessageItemNotYours {...ItemProps} />;
+            })}
         </div>
-      </Container>
-    );
-}
+
+        <InputTextArea sendMessage={sendMessageHandler} />
+      </div>
+    </Container>
+  );
+};
 
 function mapStateToProps(state) {
   return {
@@ -103,6 +88,6 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return {}
+  return {};
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Dialog)
+export default connect(mapStateToProps, mapDispatchToProps)(Dialog);
