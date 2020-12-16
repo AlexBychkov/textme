9ce86dd327dc26f94123a06bb4c180dd061c092c
Vitek Foxprogs <foxprogs@gmail.com>
2020-12-17 00:53:12 +0400

    fix warnings

--- src/components/map/AddLocation.js
+++ src/components/map/AddLocation.js
@@ -3,44 +3,40 @@ import React, { useEffect, useState } from 'react';
 import AddLocationIcon from '@material-ui/icons/AddLocation';
 
 export default function AddLocation(props) {
-    const [position, setPosition] = useState({});
-    const [error, setError] = useState(null);
+  const [position, setPosition] = useState({});
+  const [, setError] = useState(null);
 
-    const defaultSettings = {
-        enableHighAccuracy: false,
-        timeout: Infinity,
-        maximumAge: 0,
-      };
+  const defaultSettings = {
+    enableHighAccuracy: false,
+    timeout: Infinity,
+    maximumAge: 0,
+  };
 
-    const onChange = ({coords}) => {
-        setPosition({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-        });
-      };
-    
-      const onError = (error) => {
-        setError(error.message);
-      };
-    
-     const handlerPosition = () => {
-        if (!navigator || !navigator.geolocation) {
-          setError('Geolocation is not supported');
-          return;
-        }
-        navigator.geolocation.getCurrentPosition(onChange, onError, defaultSettings);
+  const onChange = ({ coords }) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    });
+  };
+
+  const onError = (error) => {
+    setError(error.message);
+  };
+
+  const handlerPosition = () => {
+    if (!navigator || !navigator.geolocation) {
+      setError('Geolocation is not supported');
+      return;
     }
+    navigator.geolocation.getCurrentPosition(onChange, onError, defaultSettings);
+  };
 
-    useEffect(() => {
-     if (position.latitude) {
-      props.createMessage('location',false, position);
-     }
-    }, [position.latitude])
-   
+  useEffect(() => {
+    if (position.latitude) {
+      props.createMessage('location', false, position);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [position.latitude]);
 
-    return (
-        <AddLocationIcon onClick={handlerPosition}></AddLocationIcon>
-    );
+  return <AddLocationIcon onClick={handlerPosition}></AddLocationIcon>;
 }
-
-
